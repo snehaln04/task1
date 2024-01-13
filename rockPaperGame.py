0bd29bab117897e4785489651704b84ec87399af
snehaln04 <156079782+snehaln04@users.noreply.github.com>
2024-01-13 09:36:10 +0530

    Update rockPaperGame.py

--- rockPaperGame.py
+++ rockPaperGame.py
@@ -1,84 +1,64 @@
 import random
-while True:
 
- user_score = 0 
- comp_score = 0
- ties = 0
+user_score = 0
+comp_score = 0
+ties = 0
 
- print("Rules for the game: ")
- print("""
+print("Rules for the game: ")
+print("""
     1. Paper VS Rock ---> Paper
     2. Rock VS Scissors ---> Rock
     3. Scissosrs VS Paper ---> Scissors """)
- 
- print(""" Choices are -
-      1. ROCK
-      2. PAPER
-      3. SCISSORS""")
- choice = int(input("enter your choice from 1-3 : "))
- print()
 
- while(choice > 3 or choice <1):
-  choice= int(input("please enter valid input"))
- if(choice==1):
-    user_choice = "Rock"
+while True:
+    print(""" Choices are -
+          1. ROCK
+          2. PAPER
+          3. SCISSORS""")
+    choice = int(input("Enter your choice from 1-3 : "))
+    print()
 
- elif(choice==2):
-   user_choice = "Paper"
- else:
-   user_choice = "Scissors"
- print("user's choice is ", user_choice)
- print()
- print("now its computer turn.....")
- print()
+    while choice > 3 or choice < 1:
+        choice = int(input("Please enter a valid input: "))
 
- computer = random.randint(1,3)
- if(computer == 1):
-  comp_choice = "Rock"
- elif(computer == 2):
-  comp_choice = "Paper"
- else:
-  comp_choice = "Scissors"
- print("computer choice is ", comp_choice)
- print()
+    if choice == 1:
+        user_choice = "Rock"
+    elif choice == 2:
+        user_choice = "Paper"
+    else:
+        user_choice = "Scissors"
 
+    print("User's choice is ", user_choice)
+    print("Now it's computer's turn.....")
+    print()
 
- if(user_choice == comp_choice):
-  print("it's tie")
-  result = "tie"
- elif(user_choice =="rock" and comp_choice =="scissors")or(user_choice =="scissors" and comp_choice =="paper")or(user_choice =="paper" and comp_choice =="rock"):
-  print("you win")
-  result = "you win"
- else:
-  print( "you lose")
-  result = "you lose"
+    computer = random.randint(1, 3)
+    if computer == 1:
+        comp_choice = "Rock"
+    elif computer == 2:
+        comp_choice = "Paper"
+    else:
+        comp_choice = "Scissors"
+    print("Computer's choice is ", comp_choice)
+    print()
 
- if((user_choice == "Paper" and comp_choice == "Rock")) or ((user_choice == "rock" and comp_choice == "Paper")):
-  print("paper win")
-  result = "paper"
+    if user_choice == comp_choice:
+        print("It's a tie")
+        ties += 1
+    elif (user_choice == "Rock" and comp_choice == "Scissors") or (user_choice == "Scissors" and comp_choice == "Paper") or (user_choice == "Paper" and comp_choice == "Rock"):
+        print("You win")
+        user_score += 1
+    else:
+        print("You lose")
+        comp_score += 1
 
- elif((user_choice == "rock" and comp_choice == "scissors")) or ((user_choice == "scissors" and comp_choice == "rock")):
-  print("rock win")
-  result = "rock"
- else:
-  print("scissors win")
-  result = "scissors"
+    print("Scores are:")
+    print("Your score is: ", user_score)
+    print("Computer's score is: ", comp_score)
+    print("Ties: ", ties)
 
- if(result=="tie"):
-  ties += 1
- elif(result=="user_choice"):
-  user_score += 1
- else:
-  comp_score += 1
-
- print("scores are")
- print("your score is: ", user_score)
- print("computer score is: ", comp_score)
- print("ties are:",ties)
-
- play_again = input("Do you want to play again....(Yes/No): ")
- if(play_again=="Yes"):
-  break
- else:
-  print("GAME OVER......")
-  print("Thanks for playing.....")
+    play_again = input("Do you want to play again? (Yes/No): ")
+    if play_again.lower() != "yes":
+        print("GAME OVER......")
+        print("Thanks for playing.....")
+        break
